89097233fb356fc04d9e970db4f78a2f36872800
helloAPI <mki7778@naver.com>
2022-06-19 01:19:28 +0900

    Revert "Revert "초기 프로젝트""
    This reverts commit dfc404f9abdb1062b45586d28c3a8b8773370d9f.

---
+++ master_jun/src/test/java/com/coinmaster/master_jun/MasterJunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinmaster.master_jun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterJunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
